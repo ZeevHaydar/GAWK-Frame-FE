49645bf7e19ec84ad67d2d43c1da139ccd86e41f
Coding Miku <102744661+toper664@users.noreply.github.com>
2023-12-08 16:53:33 +0700

    feat: changed app.js routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "../src/pages/Home/Home";
+import Chat from "../src/pages/Chat/Chat";
+import Pembayaran from "../src/pages/Pembayaran/Pembayaran";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/pembayaran" element={<Pembayaran />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
