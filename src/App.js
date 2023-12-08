627502cd5b7927f1876c5bba2dac36d4ebb14ecd
Coding Miku <102744661+toper664@users.noreply.github.com>
2023-12-08 18:10:09 +0700

    fix: router fixes on App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import Home from "../src/pages/Home/Home";
 import Chat from "../src/pages/Chat/Chat";
 import Pembayaran from "../src/pages/Pembayaran/Pembayaran";
-import { Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/pembayaran" element={<Pembayaran />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/pembayaran" element={<Pembayaran />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
