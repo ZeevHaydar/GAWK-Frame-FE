dd8d8f3eab267b1bee50e52bf129caca92c5ea3b
Ivan Aldy Ganesen <18221045@std.stei.itb.ac.id>
2023-12-08 18:29:21 +0700

    fix: App.js, default root working

--- src/App.js
+++ src/App.js
@@ -5,25 +5,24 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/pembayaran" element={<Pembayaran />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/pembayaran" element={<Pembayaran />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 
 export default App;
