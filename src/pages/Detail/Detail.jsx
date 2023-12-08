1eb9f09c8b4ea08678bc1ed0b06a4e7790d6bbf5
Ivan Aldy Ganesen <18221045@std.stei.itb.ac.id>
2023-12-08 23:26:18 +0700

    fix: Cloth Detail Layout
    just need to alter the layouts with API (Backend)

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,41 +1,51 @@
 import React from 'react';
 
+const Flex = ({ children, ...props }) => (
+  <div style={{ display: 'flex', ...props }}>{children}</div>
+);
+
+const Box = ({ children, ...props }) => (
+  <div style={{ ...props }}>{children}</div>
+);
+
 const Shape = () => (
-  <div className="Shape" style={{ width: 33.33, height: 33.33, background: '#2D3648' }}></div>
+  <div className="Shape" style={{ width: '35px', height: '33.33px', background: '#2D3648' }}></div>
 );
 
 const IconImage = () => (
-  <div className="IconImage" style={{ width: 40, height: 40, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex' }}>
+  <div className="IconImage" style={{ width: '40px', height: '40px', position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex' }}>
     <Shape />
   </div>
 );
 
-const WfImagePlaceholder = ({ width, height }) => (
-  <div className="WfImagePlaceholder" style={{ width, height, background: '#EDF0F7', border: '2px #2D3648 solid', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+const PicturePlaceholder = ({ width, height }) => (
+  <div className="PicturePlaceholder" style={{ width, height, background: '#EDF0F7', border: '2px #2D3648 solid', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
     <IconImage />
   </div>
 );
 
-const Slot2 = ({ width, height }) => (
-  <div className="Slot2" style={{ flex: '1 1 0', alignSelf: 'stretch', background: 'white', borderRadius: 8, overflow: 'hidden', border: '2px #2D3648 solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex' }}>
-    <WfImagePlaceholder width={width} height={height} />
+const PicturePosition = ({ width, height }) => (
+  <div className="PicturePosition" style={{ flex: '1 1 0', alignSelf: 'stretch', background: 'white', borderRadius: '15px', overflow: 'hidden', border: '2px #2D3648 solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex' }}>
+    <PicturePlaceholder width={width} height={height} />
   </div>
 );
 
-const WfSlotColumns = ({ width, height, paddingTop, left, top }) => (
-  <div className="WfSlotColumns" style={{ width, height, paddingTop, left, top, position: 'absolute', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 24, display: 'inline-flex' }}>
-    <Slot2 width={width} height={height} />
+const PictureSlots = ({ width, height, paddingTop, left, top }) => (
+  <div className="PictureSlots" style={{ width, height, paddingTop, left, top, position: 'absolute', justifyContent: 'flex-start', alignItems: 'flex-start', gap: '25px', display: 'inline-flex' }}>
+    <PicturePosition width={width} height={height} />
   </div>
 );
 
 const ScreenClothesDescription = () => (
-    <div className="ScreenClothesDescriptionDoNotChangeLiverNoMore" style={{ width: 1440, height: 1024, position: 'relative', background: 'white' }}>
-      <WfSlotColumns width={319} height={610} paddingTop={40} left={31} top={0} />
-      <WfSlotColumns width={673} height={370} paddingTop={40} left={31} top={609} />
-      <WfSlotColumns width={319} height={610} paddingTop={40} left={385} top={-1} />
-      <div className="ShortDress" style={{ left: 739, top: 65, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 40, fontFamily: 'Inter', fontWeight: '700', lineHeight: 24, wordWrap: 'break-word' }}>Short Dress</div>
+  <Flex>
+    <Box style={{ width: '100vw', height: '71vw', position: 'relative', background: 'white' }}>
+      <PictureSlots width={'400px'} height={'550px'} paddingTop={'10px'} left={'30px'} top={'40px'} />
+      {/* <PictureSlots width={'22.15vw'} height={'35vw'} paddingTop={'2.78vw'} left={'26.74vw'} top={'5vw'} /> */}
+      {/* <PictureSlots width={'46.74vw'} height={'25.69vw'} paddingTop={'2.78vw'} left={'2.15vw'} top={'30vw'} /> */}
+      <div className="ShortDress" style={{ left: '500px', top: '50px', position: 'absolute', textAlign: 'center', color: 'black', fontSize: '40px', fontFamily: 'Inter', fontWeight: '700', lineHeight: '42px', wordWrap: 'break-word' }}>Short Dress</div>
       {/* Other components go here */}
-    </div>
-  );
-  
-  export default ScreenClothesDescription;
+    </Box>
+  </Flex>
+);
+
+export default ScreenClothesDescription;
